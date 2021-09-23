49d182ea39521d51027efb45bf0a2e4e9d699654
shreekanth11 <84184831+shreekanth11@users.noreply.github.com>
2021-09-23 17:26:42 +0530

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,29 @@
+import pickle
+import numpy as np
+import pandas as pd
+from joblib import dump,load
+
+model= pickle.load(open('model_ml7.pkl','rb'))
+convert=load('poly_convert.joblib')
+
+def predict(df):
+    df=df[["RD_Spend","Administration","Marketing_Spend","State"]]
+    df.drop('Administration',axis=1,inplace=True)
+    df["State_Florida"]=df["State"]
+    df["State_Newyork"]=df["State"]
+    for i in range(len(df.State)):
+        if(df.State[i]=='Florida'):
+            df.State_Florida[i]=1
+            df.State_Newyork[i]=0
+        else:
+            df.State_Florida[i]=0
+            df.State_Newyork[i]=1
+    df.drop('State',axis=1,inplace=True)
+    a=convert.fit_transform(df)
+    numpy_array = a.to_numpy()
+    predictions = model.predict(numpy_array)
+    output = (np.around(predictions)).tolist()
+    sent = "Predicted Price is: "
+    output = [str(i) for i in output]
+    output = ["{}{}".format(sent , i) for i in output]
+    return output
